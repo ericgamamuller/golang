d0992f65a578f91abc57c216852906b065c8261a
ericgamamuller <ericgamamuller@gmail.com>
2020-04-06 17:00:24 -0300

    Changing visuals, program flow and renaming to English

---
+++ challenge-01/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println("+ ===== - ===== - ===== - ===== - ===== - ===== +")
+	fmt.Println(":          Challenge 01 - Healthcheck           :")
+	fmt.Println("+ ===== - ===== - ===== - ===== - ===== - ===== +")
+	fmt.Println()
+
+	for {
+		showMenu()
+		runCommand(getCommand())
+	}
+}
+
+// Menu
+func showMenu() {
+	fmt.Println("  ----- MENU -----")
+	fmt.Println("    1 - Healthcheck [Sandbox]")
+	fmt.Println("    2 - Healthcheck [Production]")
+	fmt.Println("    0 - Exit")
+	fmt.Println()
+}
+
+func getCommand() int {
+	var command int
+	fmt.Print("Choice: ")
+	fmt.Scan(&command)
+	fmt.Println()
+	return command
+}
+
+func runCommand(command int) {
+	switch command {
+	case 1:
+		fmt.Println("Running: Healthcheck [Sandbox]")
+		healthcheckSandbox()
+	case 2:
+		fmt.Println("Running: Healthcheck [Production]")
+		healthcheckProduction()
+	case 0:
+		fmt.Println("Thanks for using this program!")
+		os.Exit(1)
+	default:
+		fmt.Println("Unknown command")
+	}
+
+	fmt.Println()
+	fmt.Println()
+}
+
+// Commands
+func healthcheckSandbox() {
+	urls := read("urls.txt")
+	runHealthchecks(urls)
+}
+
+func healthcheckProduction() {
+	urls := read("urls.txt")
+	runHealthchecks(urlsRemoveSandbox(urls))
+}
+
+// Functions
+func urlsRemoveSandbox(urls []string) []string {
+	for i, url := range urls {
+		urls[i] = strings.Replace(url, "sandbox", "", 1)
+	}
+	return urls
+}
+
+func runHealthchecks(urls []string) {
+	for _, url := range urls {
+		check := healtcheck(url)
+		if check {
+			fmt.Println("[ OK ] " + url)
+		} else {
+			fmt.Println("[FAIL] " + url)
+		}
+	}
+}
+
+// File Management
+func read(filename string) []string {
+	var content []string
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println("Error reading file '"+filename+"':", err)
+		return content
+	}
+
+	reader := bufio.NewReader(file)
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading file '"+filename+"':", err)
+			return content
+		}
+		content = append(content, strings.TrimSpace(string(line)))
+	}
+
+	return content
+}
+
+// Healthcheck
+type healtcheckResponse struct {
+	IsHealthy bool
+}
+
+func healtcheck(url string) bool {
+	response, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Error calling '"+url+"':", err)
+		return false
+	}
+
+	if response.StatusCode != 200 {
+		return false
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		fmt.Println("Error reading '"+url+"' response:", err)
+		return false
+	}
+
+	var bodyObject healtcheckResponse
+	json.Unmarshal(body, &bodyObject)
+
+	return bodyObject.IsHealthy
+}
